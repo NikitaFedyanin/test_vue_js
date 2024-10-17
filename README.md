c9d087f8777fdc11973cbd3e1d68570731c72281
Никита <nikitafedyanin19@gmail.com>
2024-10-18 00:13:02 +0300

    add files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# test_vue_js
-code of my education upon vue js
+# my-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-btn color="white" :text-color="btn_color" :label="btn_label" v-on:click="switcher"/>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const btn_label = ref("Primary")
+const btn_color = ref("secondary")
+
+function switcher () {
+  if (btn_label.value === "Primary"){
+    btn_label.value = "Amber"
+    btn_color.value = "purple"
+  } else {
+    btn_label.value = "Primary"
+    btn_color.value = "secondary"
+  }
+}
+
+</script>
