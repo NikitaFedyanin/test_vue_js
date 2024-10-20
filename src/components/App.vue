c854b82595261ec8ed7112714cb24a34ff5e0232
Никита <nikitafedyanin19@gmail.com>
2024-10-20 17:10:36 +0300

    second

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,20 +1,34 @@
 <template>
-  <q-btn color="white" :text-color="btn_color" :label="btn_label" v-on:click="switcher"/>
+  <q-banner v-if="is_primary" rounded class="bg-red-8 text-white">Включен Primary</q-banner>
+  <q-btn class="fixed-center" color="white" v-bind="btn_attrs" :label="btn_label" @click="switcher"/>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
+// reactive работает с Map, List, делая сам объект реактивным
+const btn_attrs = reactive({
+  'text-color': "secondary",
+})
+
+// Оборачивает значение в реактивный объект
 const btn_label = ref("Primary")
-const btn_color = ref("secondary")
+
+
+// computed возвращает ref вычисляемого свойства
+const is_primary = computed(() => {
+  return btn_label.value === "Primary" ? true : false
+})
+
+
 
 function switcher () {
   if (btn_label.value === "Primary"){
     btn_label.value = "Amber"
-    btn_color.value = "purple"
+    btn_attrs["text-color"] = "purple"
   } else {
     btn_label.value = "Primary"
-    btn_color.value = "secondary"
+    btn_attrs["text-color"] = "secondary"
   }
 }
 
